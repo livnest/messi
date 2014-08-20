14fd8a8b7310313018481d5ba412b1200df34149
Okasita Masahiro <okashita@livnest.co.jp>
2014-08-20 23:17:39 +0900

    tableviewを加えた

---
+++ CamBinder/CaTabelViewController.h
@@ -0,0 +1,13 @@
+//
+//  CaTabelViewController.h
+//  CamBinder
+//
+//  Created by Okasita Masahiro on 2014/08/20.
+//  Copyright (c) 2014年 MatsumotoTakuma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CaTabelViewController : UIViewController
+
+@end
