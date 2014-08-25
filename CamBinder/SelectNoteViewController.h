9eee7ae2b7eab99dac11924f7fc88c96dc094516
MatsuTaku <matsumoto@livnest.co.jp>
2014-08-25 17:40:04 +0900

    SelectNoteViewControllerの追加

---
+++ CamBinder/SelectNoteViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectNoteViewController.h
+//  CamBinder
+//
+//  Created by 松本拓真 on 2014/08/25.
+//  Copyright (c) 2014年 MatsumotoTakuma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectNoteViewController : UITableViewController
+
+@end
