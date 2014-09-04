029de2806ac1f07537eee53b7aaa8f4518bf2a7d
MatsuTaku <matsumoto@livnest.co.jp>
2014-09-05 00:20:14 +0900

    AlertViewの実装をリセット

--- CamBinder/CaTabelViewController.m
+++ CamBinder/CaTabelViewController.m
@@ -9,8 +9,10 @@
 #import "CaTabelViewController.h"
 
 @interface CaTabelViewController () <UITableViewDelegate,UITableViewDataSource>
+
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property (nonatomic, strong) NSArray *dataSourceiPhone;
+@property (nonatomic, strong) NSArray *dataSourceSubject;
+
 @end
 
 @implementation CaTabelViewController
@@ -31,9 +33,10 @@
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     // テーブルに表示したいデータソースをセット
-    self.dataSourceiPhone = @[ ];
+    self.dataSourceSubject = @[@"授業1"];
     
 }
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"Cell";
@@ -44,48 +47,57 @@
                                       reuseIdentifier:CellIdentifier];
     }
     
-    switch (indexPath.section) {
-        case 0:
-            cell.textLabel.text = self.dataSourceiPhone[indexPath.row];
-            break;
-       
-        default:
-            break;
-    }
+    cell.textLabel.text = self.dataSourceSubject[indexPath.row];
     return cell;
 
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 2;
+    return 1;
 }
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     NSInteger dataCount;
     
-    // テーブルに表示するデータ件数を返す
-    switch (section) {
-        case 0:
-            dataCount = self.dataSourceiPhone.count;
-            break;
-                default:
-            break;
-    }
+//    // テーブルに表示するデータ件数を返す
+//    switch (section) {
+//        case 0:
+//            dataCount = self.dataSourceSubject.count;
+//            break;
+//                default:
+//            break;
+//    }
+    dataCount = self.dataSourceSubject.count;
     return dataCount;
 }
 
-- (IBAction)MakeSubjectButton:(id)sender {
-    UIAlertView *Alert = [[UIAlertView alloc] initWithTitle:@"新規授業名" message:@"授業名を入力してください。" delegate:self cancelButtonTitle:@"キャンセル" otherButtonTitles:@"保存", nil];
-    [Alert setAlertViewStyle:UIAlertViewStylePlainTextInput];
-    [Alert show];
-}
-
--(void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex{
-    if (buttonIndex==1) {
-        NSLog(@"%@",[[alertView textFieldAtIndex:0] text]);
-    }
-}
+//- (IBAction)MakeSubjectButton:(id)sender {
+//    // UITableViewの初期化
+//    UIAlertView *Alert = [[UIAlertView alloc] initWithTitle:@"新規授業名"
+//                                                    message:@"授業名を入力してください。"
+//                                                   delegate:self
+//                                          cancelButtonTitle:@"キャンセル"
+//                                          otherButtonTitles:@"保存", nil
+//                          ];
+//    [Alert setAlertViewStyle:UIAlertViewStylePlainTextInput];
+//    [Alert show];
+//}
+//
+//// AlertViewが終了した後
+//-(void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex{
+//    NSMutableArray *dataSourceSubject = [NSMutableArray array];
+//    if (buttonIndex==1) {
+//        NSLog(@"%@",[[alertView textFieldAtIndex:0] text]);
+//        [dataSourceSubject addObject:[[alertView textFieldAtIndex:0] text]];
+//    }
+//}
+//
+//// AlertViewのボタンを使えるようにする
+//- (BOOL)alertViewShouldEnableFirstOtherButton:(UIAlertView *)alertView{
+//    return YES;
+//}
 
 @end
 
