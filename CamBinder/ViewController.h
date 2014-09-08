e1c027461dba352756aa59fddf74f3bfa2e9dda3
takumahoriuchi <horitaku46@gmail.com>
2014-09-08 23:09:42 +0900

    ファイルを移動

---
+++ CamBinder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CamBinder
+//
+//  Created by 松本拓真 on 2014/07/19.
+//  Copyright (c) 2014年 MatsumotoTakuma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
